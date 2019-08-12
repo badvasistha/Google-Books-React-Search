dd68e777312149ba2f08b42d19222f9a51d5df64
Badri Vasistha <badri.vasistha@gmail.com>
2019-08-11 19:50:13 -0700

    seed added into package.json scripts.

---
+++ client/build/precache-manifest.22fc1cc2f9628e66c3fe652631dd183b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "de1795a5ab3793c330cd",
+    "url": "/static/css/main.9ddd8005.chunk.css"
+  },
+  {
+    "revision": "de1795a5ab3793c330cd",
+    "url": "/static/js/main.3a3aa2d4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1614f318a69fb127bdac",
+    "url": "/static/js/2.e1c36244.chunk.js"
+  },
+  {
+    "revision": "516d71ec1029d07514f0e0d9c53b86eb",
+    "url": "/index.html"
+  }
+];
